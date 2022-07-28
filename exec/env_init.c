280c4a5e63a58845e4eff0aa59b63607067e25a9
ilahyani <lahyani799@gmail.com>
2022-07-28 03:55:31 +0100

    env_init: incrementing shlvl

--- exec/env_init.c
+++ exec/env_init.c
@@ -6,13 +6,30 @@
 /*   By: ilahyani <ilahyani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 15:57:15 by ilahyani          #+#    #+#             */
-/*   Updated: 2022/07/26 01:57:12 by ilahyani         ###   ########.fr       */
+/*   Updated: 2022/07/28 03:54:23 by ilahyani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
 //TODO: unset oldpwd & export oldpwd(empty) & export pwd=getcwd & increment shlvl
+
+void	update_shlvl(t_env **lst_env)
+{
+	t_env 	*tmp;
+	char	*lvl;
+
+	if (!*lst_env)
+		return ;
+	tmp = env_lstlast(*lst_env);
+	if (!ft_strcmp(tmp->var, "SHLVL"))
+	{
+		lvl = ft_strdup(tmp->value);
+		free(tmp->value);
+		tmp->value = ft_itoa(ft_atoi(lvl) + 1);
+		free(lvl);
+	}
+}
 
 void	env_init(char **env, t_env **lst_env)
 {
@@ -28,6 +45,8 @@ void	env_init(char **env, t_env **lst_env)
         if (!buff[1])
             buff[1] = NULL;
 	    env_lstadd_back(lst_env, env_lstnew(buff[0], buff[1]));
+		if (!ft_strcmp(buff[0], "SHLVL"))
+			update_shlvl(lst_env);
 		free_tab(buff);
     }
 }
