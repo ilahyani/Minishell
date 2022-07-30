4136b664b30cd279819dce91a2bfce5c031b8720
Ismail Lahyani <ilahyani@e3r1p3.1337.ma>
2022-07-30 03:58:44 +0100

    ft_pipe: broken pipe issue fixed

--- exec/pipe.c
+++ exec/pipe.c
@@ -63,19 +63,20 @@ void    exec_child(t_node *node, t_env *lst_env, int fd[2], int s_in)
 {
     if (check_redir(node))
     {
-        close(fd[0]);
         close(fd[1]);
+        close(fd[0]);
         if (check_heredoc(node))
             dup2(s_in, STDIN_FILENO);
         redir_io(node, lst_env);
     }
     else
     {
-        if (!is_last(node)) //not last cmd
+        if (!is_last(node))
             dup2(fd[1], STDOUT_FILENO);
         close(fd[1]);
         close(fd[0]);
         check_cmd(node->cmd, lst_env);
+
     }
     exit(g_exit);
 }
@@ -95,15 +96,30 @@ void    s_in_reset(int s_in)
     close(s_in);
 }
 
+void    set_status(int j)
+{
+    int status;
+
+    while (j--)
+    {
+        wait(&status);
+        printf("->%d\n", WEXITSTATUS(status));
+    }
+    g_exit = WEXITSTATUS(status);
+}
+
 int ft_pipe(t_node *node, t_env *lst_env)
 {
     int     fd[2];
     pid_t   c_pid;
     int     s_in;
+    int     j;
     
     s_in = dup(0);
+    j = 0;
     while (node)
     {
+        j++;
         if (pipe(fd) == -1)
             return (1);
         c_pid = fork();
@@ -111,11 +127,11 @@ int ft_pipe(t_node *node, t_env *lst_env)
             return(ft_putstr_fd("fork error\n", 2), 1);
         else if (c_pid == 0)
             exec_child(node, lst_env, fd, s_in);
-        wait(NULL); // wait only for the last cmd?
         dup2(fd[0], STDIN_FILENO);
         close(fd[0]);
         close(fd[1]);
         next_cmd(&node);
     }
-    return (s_in_reset(s_in), 0);
+    set_status(j);
+    return (s_in_reset(s_in), g_exit);
 }
