d98688f66b03b4556c37ad5fbf73bd23446177bc
ilahyani <lahyani799@gmail.com>
2022-07-31 07:00:21 +0100

    utils functions separation

--- exec/utils.c
+++ exec/utils.c
@@ -6,43 +6,11 @@
 /*   By: ilahyani <ilahyani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 10:28:22 by ilahyani          #+#    #+#             */
-/*   Updated: 2022/07/25 23:13:18 by ilahyani         ###   ########.fr       */
+/*   Updated: 2022/07/31 06:59:52 by ilahyani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
-
-int	ft_strcmp(char *s1, char *s2)
-{
-	size_t	i;
-
-	i = 0;
-	while (s1[i] && s2[i] && s1[i] == s2[i])
-		i++;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-}
-
-int	sizeof_array(char **arr)
-{
-	int	i;
-
-	i = -1;
-	while (arr[++i]);
-	return (i);
-}
-
-int	is_int(char *c)
-{
-	int	i;
-
-	i = -1;
-	if (c[0] == '-' || c[0] == '+')
-		i++;
-	while (c[++i])
-		if (c[i] < '0' || c[i] > '9')
-			return (0);
-	return (1);
-}
 
 char	*strchr_plus(const char *s, int c)
 {
@@ -103,13 +71,4 @@ void	free_tab(char **tab)
 	while (tab[++i])
 		free(tab[i]);
 	free(tab);
-}
-
-char    *ft_getenv(char *env, t_env *env_list)
-{
-    while (env_list && ft_strcmp(env, env_list->var))
-        env_list = env_list->next;
-    if (env_list)
-        return (env_list->value);
-    return (NULL);
 }
---
+++ exec/utils_2.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_2.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilahyani <ilahyani@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/31 06:58:54 by ilahyani          #+#    #+#             */
+/*   Updated: 2022/07/31 06:59:49 by ilahyani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	size_t	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+int	sizeof_array(char **arr)
+{
+	int	i;
+
+	i = -1;
+	while (arr[++i]);
+	return (i);
+}
+
+int	is_int(char *c)
+{
+	int	i;
+
+	i = -1;
+	if (c[0] == '-' || c[0] == '+')
+		i++;
+	while (c[++i])
+		if (c[i] < '0' || c[i] > '9')
+			return (0);
+	return (1);
+}
+
+char    *ft_getenv(char *env, t_env *env_list)
+{
+    while (env_list && ft_strcmp(env, env_list->var))
+        env_list = env_list->next;
+    if (env_list)
+        return (env_list->value);
+    return (NULL);
+}
