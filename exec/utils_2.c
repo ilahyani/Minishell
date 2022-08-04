b24b30aa6746c7dc72248e541713554e2a05b517
ilahyani <lahyani799@gmail.com>
2022-08-04 19:04:24 +0100

    exec/my_cd.c
    cd sigfault fixed

--- exec/utils_2.c
+++ exec/utils_2.c
@@ -6,7 +6,7 @@
 /*   By: ilahyani <ilahyani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/31 06:58:54 by ilahyani          #+#    #+#             */
-/*   Updated: 2022/07/31 23:21:23 by ilahyani         ###   ########.fr       */
+/*   Updated: 2022/08/04 18:59:16 by ilahyani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_strcmp(char *s1, char *s2)
 	size_t	i;
 
 	i = 0;
+	if (!s1 || !s2)
+		return (-1);
 	while (s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
