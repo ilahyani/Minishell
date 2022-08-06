3521a20b54c5542d7b8435fc14780c39e2f7ca51
ilahyani <lahyani799@gmail.com>
2022-08-06 17:28:14 +0100

    unset shlvl bug fixed

--- exec/env_init.c
+++ exec/env_init.c
@@ -6,7 +6,7 @@
 /*   By: ilahyani <ilahyani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 15:57:15 by ilahyani          #+#    #+#             */
-/*   Updated: 2022/08/01 00:37:52 by ilahyani         ###   ########.fr       */
+/*   Updated: 2022/08/06 17:24:39 by ilahyani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,10 @@ void	env_init(char **env, t_env **lst_env)
 			env_lstadd_back(lst_env, env_lstnew(buff[0], ""));
 		else
 			env_lstadd_back(lst_env, env_lstnew(buff[0], buff[1]));
-		update_env_var(lst_env);
 		free_tab(buff);
 	}
+	update_shlvl(lst_env);
+	update_pwd(lst_env);
 	unset_oldpwd(*lst_env);
 }
 
@@ -51,14 +52,16 @@ void	unset_oldpwd(t_env *lst_env)
 	}
 }
 
-void	update_env_var(t_env **lst_env)
+void	update_shlvl(t_env **lst_env)
 {
 	t_env	*tmp;
 	char	*lvl;
 	int		new_lvl;
 
-	tmp = env_lstlast(*lst_env);
-	if (!ft_strcmp(tmp->var, "SHLVL"))
+	tmp = *lst_env;
+	while (tmp && ft_strcmp(tmp->var, "SHLVL"))
+		tmp = tmp->next;
+	if (tmp)
 	{
 		lvl = ft_strdup(tmp->value);
 		free(tmp->value);
@@ -68,7 +71,18 @@ void	update_env_var(t_env **lst_env)
 		tmp->value = ft_itoa(new_lvl);
 		free(lvl);
 	}
-	else if (!ft_strcmp(tmp->var, "PWD"))
+	else
+		env_lstadd_back(lst_env, env_lstnew("SHLVL", "1"));
+}
+
+void	update_pwd(t_env **lst_env)
+{
+	t_env	*tmp;
+
+	tmp = *lst_env;
+	while (tmp && ft_strcmp(tmp->var, "PWD"))
+		tmp = tmp->next;
+	if (tmp)
 	{
 		if (tmp->value)
 		{
@@ -76,4 +90,6 @@ void	update_env_var(t_env **lst_env)
 			tmp->value = getcwd(NULL, sizeof(NULL));
 		}
 	}
+	else
+		env_lstadd_back(lst_env, env_lstnew("PWD", getcwd(NULL, sizeof(NULL))));
 }
