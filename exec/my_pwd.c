b6e11cdae70597b1623e5b64dfa35d1219223beb
ilahyani <lahyani799@gmail.com>
2022-08-08 06:45:17 +0100

    my_pwd fix

--- exec/my_pwd.c
+++ exec/my_pwd.c
@@ -6,7 +6,7 @@
 /*   By: ilahyani <ilahyani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/26 12:19:57 by ilahyani          #+#    #+#             */
-/*   Updated: 2022/08/08 06:14:48 by ilahyani         ###   ########.fr       */
+/*   Updated: 2022/08/08 06:44:04 by ilahyani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	my_pwd(void)
 	char	*cwd;
 
 	cwd = NULL;
-	getcwd(cwd, sizeof(cwd));
+	cwd = getcwd(NULL, sizeof(NULL));
 	printf("%s\n", cwd);
 	free(cwd);
 	return (0);
