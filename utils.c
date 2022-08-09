c95979d6275d1b78ab706e29adb74ab5a980a956
ilahyani <lahyani799@gmail.com>
2022-08-09 22:44:06 +0100

    added main utils

---
+++ utils.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilahyani <ilahyani@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/09 22:40:21 by ilahyani          #+#    #+#             */
+/*   Updated: 2022/08/09 22:41:12 by ilahyani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	ft_readline(t_env **lst_env)
+{
+	char	*line;
+	t_node	*cmd;
+
+	while (42)
+	{
+		set_signals("main");
+		line = readline("🐌🐌🐌 ~ ");
+		if (!line)
+		{
+			g_status = 0;
+			break ;
+		}
+		if (empty_line(line))
+			continue ;
+		cmd = parser(line, *lst_env);
+		if (cmd)
+			interpret_cmd(cmd, lst_env);
+		else
+			cmd_error(cmd);
+		if (ft_strlen(line) > 0)
+			add_history(line);
+		free(line);
+	}
+	free(line);
+	printf("ight I'mma head out\n");
+}
+
+void	interpret_cmd(t_node *cmd, t_env **lst_env)
+{
+	int		s_fd[2];
+	int		status;
+	t_node	*lst;
+
+	lst = cmd;
+	s_fd[1] = dup(1);
+	s_fd[0] = dup(0);
+	status = 0;
+	if (find_char_2(cmd, PIPE))
+		g_status = exec_pipe(cmd, lst_env);
+	else
+	{
+		if (find_char_2(cmd, OUT_REDIR) || find_char_2(cmd, IN_REDIR)
+			|| find_char_2(cmd, RE_ADD) || find_char_2(cmd, HERE_DOC))
+			status = redir_io(cmd, *lst_env);
+		if (!status && cmd->type == WORD)
+			check_cmd(cmd->cmd, lst_env);
+		else if (status)
+			g_status = 1;
+	}
+	fd_reset(s_fd);
+	free_cmd(lst);
+}
+
+int	exec_pipe(t_node *cmd, t_env **lst_env)
+{
+	int	status;
+
+	status = ft_pipe(cmd, *lst_env);
+	if (status)
+		return (1);
+	return (0);
+}
+
+void	check_cmd(char **cmd, t_env **lst_env)
+{
+	if (!ft_strcmp(cmd[0], "pwd"))
+		g_status = my_pwd(*lst_env);
+	else if (!ft_strcmp(cmd[0], "cd"))
+		g_status = my_cd(*lst_env, cmd);
+	else if (!ft_strcmp(cmd[0], "echo"))
+		g_status = my_echo(cmd);
+	else if (!ft_strcmp(cmd[0], "export"))
+		g_status = my_export(cmd, lst_env);
+	else if (!ft_strcmp(cmd[0], "env"))
+		g_status = my_env(*lst_env, cmd);
+	else if (!ft_strcmp(cmd[0], "unset"))
+		g_status = my_unset(lst_env, cmd);
+	else if (!ft_strcmp(cmd[0], "exit"))
+		my_exit(cmd);
+	else
+		g_status = ft_exec(cmd, *lst_env);
+}
+
+int	empty_line(char *line)
+{
+	if (!ft_strcmp(line, ""))
+		return (free(line), 1);
+	return (0);
+}
