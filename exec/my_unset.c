4c739b26fdda3839d9a641fbd324a2ba2b563f02
ilahyani <lahyani799@gmail.com>
2022-08-09 21:50:43 +0100

    my_unset error_check and multi error check bug fixed

--- exec/my_unset.c
+++ exec/my_unset.c
@@ -6,7 +6,7 @@
 /*   By: ilahyani <ilahyani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/26 12:20:18 by ilahyani          #+#    #+#             */
-/*   Updated: 2022/08/09 16:44:31 by ilahyani         ###   ########.fr       */
+/*   Updated: 2022/08/09 21:32:51 by ilahyani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,12 @@ int	my_unset(t_env **env, char **data)
 	i = 0;
 	while (data[++i])
 	{
-		if (find_char(data[i], '=') || (data[i][0] >= '0' && data[i][0] <= '9'))
+		if (unset_error_check(data[i]))
 		{
 			ft_putstr_fd("minishell: unset: ", 2);
 			ft_putstr_fd(data[i], 2);
 			ft_putstr_fd(": not a valid identifier\n", 2);
+			g_status = 1;
 			continue ;
 		}
 		if ((*env)->var && !ft_strcmp((*env)->var, data[i]))
@@ -33,7 +34,7 @@ int	my_unset(t_env **env, char **data)
 		else
 			remove_node(env, data[i]);
 	}
-	return (0);
+	return (g_status);
 }
 
 void	remove_head(t_env **env)
@@ -45,6 +46,7 @@ void	remove_head(t_env **env)
 	free((*env)->value);
 	free(*env);
 	(*env) = tmp;
+	g_status = 0;
 }
 
 void	remove_node(t_env **env, char *data)
@@ -63,4 +65,20 @@ void	remove_node(t_env **env, char *data)
 		free(tmp2->value);
 		free(tmp2);
 	}
+	g_status = 0;
+}
+
+int	unset_error_check(char *var)
+{
+	int	i;
+
+	if (!ft_isalpha(var[0]) && var[0] != '_')
+		return (1);
+	i = 0;
+	while (var[++i])
+	{
+		if (!ft_isalnum(var[i]) && var[i] != '_' && var[i])
+			return (1);
+	}
+	return (0);
 }
